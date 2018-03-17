e72ace41bcbf4eecfdb2458c92f1ad423dd0c904
Juan M. Uys <opyate@gmail.com>
2018-03-17 21:15:33 +0000

    more lenient; doc

---
+++ README.md
@@ -0,0 +1,1 @@
+Something to put behind IFTTT (camera widget) and ngrok to turn your static site generator into your own Fakestagram.
--- convert.py
+++ convert.py
@@ -1,5 +1,5 @@
-from PIL import Image
-
+from PIL import Image, ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 def convert(path):
     img = Image.open(path)
